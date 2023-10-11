e991337445d91d8dcdd9d1907e9451cd542fae51
Abhiramvarrey <96932995+Abhiramvarrey@users.noreply.github.com>
2023-10-11 12:42:20 +0530

    update

--- webpage/views.py
+++ webpage/views.py
@@ -3,7 +3,20 @@ from django.shortcuts import render, redirect
 from .forms import NewUserForm
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
-
+import pyrebase
+ 
+config={
+  'apiKey': "AIzaSyBH_pjeB9O6IngS9L1LdXUaj_h17LoIvk4",
+  'authDomain': "website-b995d.firebaseapp.com",
+  'projectId': "website-b995d",
+  'storageBucket': "website-b995d.appspot.com",
+  'messagingSenderId': "377928329737",
+  'appId': "1:377928329737:web:51a5cca996b905517526d8",
+  'measurementId': "G-S60Q1YJQ3Z"
+}
+firebase=pyrebase.initialize_app(config)
+authe = firebase.auth()
+database=firebase.database()
 # Create your views here.
 def home(request):
     return  render(request,'home.html')
