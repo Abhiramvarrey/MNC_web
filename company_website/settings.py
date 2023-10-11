e8915484c753d37bb6b3e9253a17d694ced02afc
Abhiramvarrey <96932995+Abhiramvarrey@users.noreply.github.com>
2023-10-11 14:13:14 +0530

    Update settings.py

--- company_website/settings.py
+++ company_website/settings.py
@@ -84,6 +84,16 @@ DATABASES = {
         'PORT': '3306',
     }
 }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.getenv('POSTGRES_DATABASE'),
+        'USER': os.getenv('POSTGRES_USER'),
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
+        'HOST': os.getenv('POSTGRES_HOST'),
+        'PORT': os.getenv('POSTGRES_PORT'),
+    }
+}
 
 
 # Password validation
